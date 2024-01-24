f0e0bfed5362e5a08ac86fd0f6d2038236653333
유호영 <yhu2121@naver.com>
2024-01-24 18:20:39 +0900

    Feat: 채팅창 화면  만드는 중

--- FrontEnd/board-a-project/src/App.jsx
+++ FrontEnd/board-a-project/src/App.jsx
@@ -1,20 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import MoimList from './routes/MoimList';
-import Home from './routes/Home';
-import React from 'react';
-import MoimDetail from './routes/MoimDetail';
-import MoimWrite from './routes/MoimWrite';
-import MoimUpdate from './routes/MoimUpdate';
+import { Route, Routes } from "react-router-dom";
+import MoimList from "./routes/MoimList";
+import Home from "./routes/Home";
+import React from "react";
+import MoimDetail from "./routes/MoimDetail";
+import MoimWrite from "./routes/MoimWrite";
+import MoimUpdate from "./routes/MoimUpdate";
+import ChatButton from "./components/chatButton/ChatButton";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/board" element={<MoimList />} />
-      <Route path="/board/:idx" element={<MoimDetail />} />
-      <Route path="/write" element={<MoimWrite />} />
-      <Route path="/update/:idx" element={<MoimUpdate />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/board" element={<MoimList />} />
+        <Route path="/board/:idx" element={<MoimDetail />} />
+        <Route path="/write" element={<MoimWrite />} />
+        <Route path="/update/:idx" element={<MoimUpdate />} />
+      </Routes>
+      <div id="chatting-ing">
+        <ChatButton></ChatButton>
+      </div>
+    </div>
   );
 }
 
