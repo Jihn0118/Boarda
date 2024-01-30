9a816ecab3d797b1a17f07767d202b3fe82834ef
sujin <ansujin0204@naver.com>
2024-01-30 18:05:37 +0900

    Refactor: 프로젝트 구조 획일화1

--- FrontEnd/board-a-project/src/App.jsx
+++ FrontEnd/board-a-project/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './routes/Home';
 import React from 'react';
 import MoimDetail from './routes/MoimDetail';
 import MoimWrite from './routes/MoimWrite';
-import MoimUpdate from './routes/MoimUpdate';
 import GameList from './routes/GameList';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Route path="/moim" element={<MoimList />} />
       <Route path="/moim/:id" element={<MoimDetail />} />
       <Route path="/write" element={<MoimWrite />} />
-      <Route path="/update/:id" element={<MoimUpdate />} />
       <Route path="/game" element={<GameList />} />
     </Routes>
   );
