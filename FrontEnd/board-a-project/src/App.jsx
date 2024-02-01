afddd6c69db2e15bad73fbdd283e40c60de737e2
유호영 <yhu2121@naver.com>
2024-02-01 09:51:21 +0900

    Feat: vite설정 추가
    - 포트 3000, 외부접속 허용, 루트 경로 @별명 추가

--- FrontEnd/board-a-project/src/App.jsx
+++ FrontEnd/board-a-project/src/App.jsx
@@ -4,10 +4,7 @@ import routes from "./routes/routes";
 import { RouterProvider } from "react-router-dom";
 
 export default function App() {
-  return(
-  <RouterProvider router={routes}>
-    <div>App afsdsa</div>
-  </RouterProvider>);
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 // import React from "react";
