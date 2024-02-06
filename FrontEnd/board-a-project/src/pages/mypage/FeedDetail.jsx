e03159ae0f72dd2b168aafea555b52a3f5be8b1e
유호영 <yhu2121@naver.com>
2024-02-06 15:51:06 +0900

    Feat: 피드상세정보페이지 수정중

--- FrontEnd/board-a-project/src/pages/mypage/FeedDetail.jsx
+++ FrontEnd/board-a-project/src/pages/mypage/FeedDetail.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
+export default function FeedDetail({
+  cafe,
+  content,
+  createdAt,
+  id,
+  isRemoved,
+  member,
+  moim,
+  rate,
+  status,
+}) {
+  // 상위에서 feed 상세정보를 넘겨받음
 
-export default function FeedDetail(props) {
-    // 상위에서 feed 상세정보를 넘겨받음 
-    
-
-
-  return (
-    <div>FeedDetail</div>
-  )
+  return <div>FeedDetail</div>;
 }
