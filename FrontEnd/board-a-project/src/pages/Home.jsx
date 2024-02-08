4d7c7623b266a2a3e63e86de0172387d2a5e2fc9
Kdh <dohoon122@gmail.com>
2024-02-08 11:09:32 +0900

    Docs: Home css 수정

--- FrontEnd/board-a-project/src/pages/Home.jsx
+++ FrontEnd/board-a-project/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       const map = new kakao.maps.Map(container, options);
       map.setZoomable(false);
     }, []);
-    return <div id="map" style={{ width: "250px", height: "200px" }}></div>;
+    return <div id="map" style={{ width: "400px", height: "300px" }}></div>;
   };
 
   return (
@@ -55,17 +55,17 @@ const Home = () => {
       </div>
       <div class="container mx-auto flex py-10">
         <div className="w-2/3 px-4">
-          <h1 class="text-2xl font-bold ...">종합 랭킹</h1>
+          <h1 class="text-2xl font-bold ...">인기 순위</h1>
 
           <div class="container flex justify-between py-5">
             <div>
-              <h2 class="text-xl ...">전체</h2>
-              <div class="py-2">
+              <h2 class="text-xl text-center ...">전체</h2>
+              <div class="py-10">
                 <ol>
                   {rankData &&
                     rankData.map((data, idx) => (
                       <li key={idx}>
-                        <span>{data}</span>
+                        <span>{idx+1}. {data}</span>
                       </li>
                     ))}
                 </ol>
@@ -73,13 +73,13 @@ const Home = () => {
             </div>
 
             <div>
-              <h2 class="text-xl ...">남성 | 여성</h2>
-              <div class="py-2">
+              <h2 class="text-xl text-center...">남성 | 여성</h2>
+              <div class="py-10">
                 <ol>
                   {rankData &&
                     rankData.map((data, idx) => (
                       <li key={idx}>
-                        <span>{data}</span>
+                        <span>{idx+1}. {data}</span>
                       </li>
                     ))}
                 </ol>
@@ -88,12 +88,12 @@ const Home = () => {
 
             <div>
               <h2 class="text-xl ...">10대 | 20대 | 30대↑</h2>
-              <div class="py-2">
+              <div class="py-10">
                 <ol>
                   {rankData &&
                     rankData.map((data, idx) => (
                       <li key={idx}>
-                        <span>{data}</span>
+                        <span>{idx+1}. {data}</span>
                       </li>
                     ))}
                 </ol>
@@ -102,8 +102,11 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="w-1/3 py-2 flex place-content-center">
-          <div>{renderMap()}</div>
+        <div className="w-1/3 px-4">
+          <h1 class="text-2xl font-bold ...">마감 임박</h1>
+          <div class="container flex justify-center py-5">
+            <div>{renderMap()}</div>
+          </div>
         </div>
       </div>
     </>
