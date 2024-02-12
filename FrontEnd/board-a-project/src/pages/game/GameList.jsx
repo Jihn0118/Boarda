65e74e95e8822fc7f8b43e2072e0cf1822d7a75e
sujin <ansujin0204@naver.com>
2024-02-12 22:04:01 +0900

    Design: 게임 리스트 디자인 수정

--- FrontEnd/board-a-project/src/pages/game/GameList.jsx
+++ FrontEnd/board-a-project/src/pages/game/GameList.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getGameList } from "../../api/gameAPI";
-import Pagination from "@mui/material/Pagination";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { gameListState } from "../../recoil/atoms/gameState";
 import GameDetail from "./GameDetail";
 
 import { CardGalaxy } from "../../mui-treasury/card-galaxy/CardGalaxy";
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import AccessibilityIcon from '@mui/icons-material/Accessibility';
-import SearchIcon from '@mui/icons-material/Search';
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import SearchIcon from "@mui/icons-material/Search";
 import Grid from "@material-ui/core/Grid";
-
 
 const GameList = () => {
   const [gameList, setGameList] = useRecoilState(gameListState);
@@ -22,34 +20,25 @@ const GameList = () => {
   const [selectedGameId, setSelectedGameId] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [totalItemsCount, setTotalItemsCount] = useState(0);
-  const [activePage, setActivePage] = useState(1);
-  const itemsCountPerPage = 12;
-
-  const currentGameList = gameList.slice(
-    (activePage - 1) * itemsCountPerPage,
-    activePage * itemsCountPerPage
-  );
-
-  const handlePageChange = async (event, pageNumber) => {
-    setActivePage(pageNumber);
-  };
+  const [displayCount, setDisplayCount] = useState(24);
 
   const handleNumChange = (selectedNum) => {
     setNum(selectedNum);
+    window.scrollTo(0, 0);
+    setDisplayCount(24);
   };
 
   const handleTimeChange = (selectedTime) => {
     setTime(selectedTime);
+    window.scrollTo(0, 0);
+    setDisplayCount(24);
   };
 
   const handleKeywordChange = (newKeyword) => {
     setKeyword(newKeyword);
+    window.scrollTo(0, 0);
+    setDisplayCount(24);
   };
-
-  useEffect(() => {
-    setTotalItemsCount(gameList.length);
-  }, [gameList]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -60,14 +49,37 @@ const GameList = () => {
     fetchData();
   }, [time, num, keyword]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 5
+      ) {
+        setDisplayCount((prevCount) => prevCount + 20);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
-      <div className="mt-4">
-        <div className="flex justify-between mx-auto mb-4" style={{ maxWidth: '1100px' }}>
-          <label className="relative block flex-grow mr-2 text-center" style={{ flex: '0.3' }}>
+      <div
+        className="mt-4"
+        style={{ position: "fixed", top: 70, width: "100%", zIndex: 1000 }}
+      >
+        <div
+          className="flex justify-between mx-auto mb-4"
+          style={{ maxWidth: "1100px" }}
+        >
+          <label
+            className="relative block flex-grow mr-2 text-center"
+            style={{ flex: "0.3" }}
+          >
             <span className="sr-only">Time:</span>
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <AccessAlarmIcon style={{ color: '#718096' }} />
+              <AccessAlarmIcon style={{ color: "#718096" }} />
             </span>
             <select
               value={time}
@@ -85,10 +97,13 @@ const GameList = () => {
             </select>
           </label>
 
-          <label className="relative block flex-grow mr-2 text-center" style={{ flex: '0.3' }}>
+          <label
+            className="relative block flex-grow mr-2 text-center"
+            style={{ flex: "0.3" }}
+          >
             <span className="sr-only">Num:</span>
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <AccessibilityIcon style={{ color: '#718096' }} />
+              <AccessibilityIcon style={{ color: "#718096" }} />
             </span>
             <select
               value={num}
@@ -108,33 +123,32 @@ const GameList = () => {
             </select>
           </label>
 
-          <label className="relative block flex-grow" style={{ flex: '1.4' }}>
+          <label className="relative block flex-grow" style={{ flex: "1.4" }}>
             <span className="sr-only">Keyword:</span>
             <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <SearchIcon style={{ color: '#718096' }} />
+              <SearchIcon style={{ color: "#718096" }} />
             </span>
-            <input 
+            <input
               value={keyword}
               onChange={(e) => handleKeywordChange(e.target.value)}
-              className="placeholder:italic placeholder:text-slate-400 block w-full bg-white border border-slate-300 rounded-md py-2 pl-10 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
-              placeholder="Search for title..." 
-              type="text" 
+              className="placeholder:italic placeholder:text-slate-400 block w-full bg-white border border-slate-300 rounded-md py-2 pl-10 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Search for title..."
+              type="text"
               name="search"
             />
           </label>
         </div>
-
       </div>
+      <div style={{ height: "100px" }}></div>
 
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Grid container spacing={3} style={{ maxWidth: "80%" }}>
-          {currentGameList.map((game) => (
+          {gameList.slice(0, displayCount).map((game) => (
             <Grid item xs={12} sm={6} md={2} key={game.id}>
               <CardGalaxy
-                // onClick={() => navigate(`/game/${game.id}`)}
                 onClick={() => {
                   setSelectedGameId(game.id);
-                  setIsModalOpen(true); 
+                  setIsModalOpen(true);
                 }}
                 title={game.title}
                 playTime={game.playTime}
@@ -144,19 +158,24 @@ const GameList = () => {
                 style={{ width: "80%", height: "80%" }}
               />
             </Grid>
-            ))}
+          ))}
         </Grid>
       </div>
 
-      <div className="flex justify-center mt-4">
-        <Pagination
-          count={Math.ceil(totalItemsCount / itemsCountPerPage)}
-          page={activePage}
-          onChange={handlePageChange}
-        />
-      </div>
+      <button
+        onClick={() => window.scrollTo(0, 0)}
+        style={{ position: "fixed", bottom: "50px", left: "50px" }}
+      >
+        맨 위로
+      </button>
 
-      {selectedGameId && <GameDetail gameId={selectedGameId} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />}
+      {selectedGameId && (
+        <GameDetail
+          gameId={selectedGameId}
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
     </div>
   );
 };
