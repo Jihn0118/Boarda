fc956f1b6bc9815efad88c4e642a37b323db0da5
김진호 <lantern50@naver.com>
2024-02-14 14:21:45 +0900

    Fix: CORS 에러 ì수정

--- BackEnd/gongtong/src/main/java/site/gongtong/config/CorsConfig.java
+++ BackEnd/gongtong/src/main/java/site/gongtong/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")   // 모든 경로를 설정
                 .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS") // 모든 http Method 허용
-                .allowedOrigins("https://www.boarda.site:3000", "http://localhost:3000", "https://boarda.site")
+                .allowedOrigins("https://www.boarda.site:3000", "http://localhost:3000", "https://www.boarda.site")
                 .allowedHeaders("jwt")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3600);
