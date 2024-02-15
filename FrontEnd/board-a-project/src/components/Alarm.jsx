452c26e2058a4d9d77ff8f56ca4d48269cf9eae7
유호영 <yhu2121@naver.com>
2024-02-15 09:16:46 +0900

    Fix: Alarm.jsx 수정

--- FrontEnd/board-a-project/src/components/Alarm.jsx
+++ FrontEnd/board-a-project/src/components/Alarm.jsx
@@ -1,21 +1,25 @@
 import Badge from "@mui/material/Badge";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { alarmState } from "../recoil/atoms/alarmState";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { loginUserState } from "../recoil/atoms/userState";
+import { useEffect } from "react";
+
+
+
 
 export default function Alarm() {
   // recoil에 저장된 alarm상태를 가져와서 구독
   // 흐름 : 로그인 요청 -> JWT발급 -> SSE요청 -> 수신 시 recoilVal갱신 -> 재랜더링
-<<<<<<< Updated upstream
-  const alarms = useRecoilValue(alarmState);
-=======
   const [alarmData, setAlarmData] = useRecoilState(alarmState);
   const loginUser = useRecoilValue(loginUserState);
+  const localURL = "http://localhost:8081/api/alarm/subscribe"
+  const remoteURL = "https://boarda.site/api/alarm/subscribe"
 
   useEffect(() => {
     // 로그인 되어있는 경우 sse 요청
     if (!!loginUser.id) {
-      const sse = new EventSource("https://boarda.site/api/alarm/subscribe");
+      const sse = new EventSource(`${remoteURL}`,{withCredentials:true});
       // 서버에서 메시지 도착 할 때
       sse.onmessage = (e) => {
         setAlarmData([...alarmData, e.data]);
@@ -36,10 +40,9 @@ export default function Alarm() {
       };
     }
   }, [loginUser]);
->>>>>>> Stashed changes
 
   return (
-    <Badge badgeContent={alarms.length}>
+    <Badge badgeContent={alarmData?.length}>
       <NotificationsIcon color="action"></NotificationsIcon>
     </Badge>
   );
