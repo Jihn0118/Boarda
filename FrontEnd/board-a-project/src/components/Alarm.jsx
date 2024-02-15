e6a8984fed99d40962d7b320dd49af26be3aaf4e
유호영 <yhu2121@naver.com>
2024-02-15 14:42:09 +0900

    Fix: SSE 연결 끊음

--- FrontEnd/board-a-project/src/components/Alarm.jsx
+++ FrontEnd/board-a-project/src/components/Alarm.jsx
@@ -14,49 +14,49 @@ export default function Alarm() {
   const localURL = "http://localhost:8081/api/alarm/subscribe";
   const remoteURL = "https://boarda.site/api/alarm/subscribe";
 
-  useEffect(() => {
-    // 로그인 되어있는 경우 sse 요청
-    if (!!loginUser.id) {
-      const sse = new EventSource(`${remoteURL}`, { withCredentials: true });
+  // useEffect(() => {
+  //   // 로그인 되어있는 경우 sse 요청
+  //   if (!!loginUser.id) {
+  //     const sse = new EventSource(`${localURL}`, { withCredentials: true });
 
-      // 연결 성공 시 메시지 수신
-      sse.addEventListener("connect", (e) => {
-        console.log(e);
-      });
+  //     // 연결 성공 시 메시지 수신
+  //     sse.addEventListener("connect", (e) => {
+  //       console.log(e);
+  //     });
 
-      // 최신알람갯수
-      sse.addEventListener("latestAlarm", (e) => {
-        console.log(e);
-      });
+  //     // 최신알람갯수
+  //     sse.addEventListener("latestAlarm", (e) => {
+  //       console.log(e);
+  //     });
 
-      // 알람갯수
-      sse.addEventListener("alarmCount", (e) => {
-        console.log(e);
-      });
+  //     // 알람갯수
+  //     sse.addEventListener("alarmCount", (e) => {
+  //       console.log(e);
+  //     });
 
-      // 새로운 알람 도착
-      sse.addEventListener("addAlarm", (e) => {
-        console.log(e);
-      });
+  //     // 새로운 알람 도착
+  //     sse.addEventListener("addAlarm", (e) => {
+  //       console.log(e);
+  //     });
 
-      // 서버에서 메시지 도착 할 때
-      sse.onmessage = (event) => {
-        console.log(`실시간알람\n${event}`);
-        console.log(event);
+  //     // 서버에서 메시지 도착 할 때
+  //     sse.onmessage = (event) => {
+  //       console.log(`실시간알람\n${event}`);
+  //       console.log(event);
 
-        setAlarmData([...alarmData, e.data]);
-      };
-      sse.onerror = (e) => {
-        console.log("sse에러");
-        console.log(e);
-      };
-      sse.onopen = async (e) => {
-        console.log("sse 연결됨");
-        await alarmAPI.getStart();
-        await alarmAPI.getList();
-      };
-    }
-  }, [loginUser.id]);
+  //       setAlarmData([...alarmData, e.data]);
+  //     };
+  //     sse.onerror = (e) => {
+  //       console.log("sse에러");
+  //       console.log(e);
+  //     };
+  //     sse.onopen = async (e) => {
+  //       console.log("sse 연결됨");
+  //       await alarmAPI.getStart();
+  //       await alarmAPI.getList();
+  //     };
+  //   }
+  // }, [loginUser.id]);
 
   return (
     <Badge badgeContent={alarmData?.length}>
